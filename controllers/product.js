48c03577fad7b3c0e6604e876e1a5965e417f144
Takeshi <heinner45@gmail.com>
2019-09-06 14:05:56 -0500

    update and delete products done

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,7 @@ const productsModel = require('../database/models/product');
 
 async function addProduct(req, res) {
     try {
-        let exist = await productsModel.findOne({id: req.body.id, pharmacy_id: req.body.pharmacy_id});
+        let exist = await productsModel.findOne({ id: req.body.id, pharmacy_id: req.body.pharmacy_id });
         if (exist) {
             return res.status(400).send({
                 ok: false,
@@ -12,7 +12,7 @@ async function addProduct(req, res) {
             });
         } else {
             let productToSave = new productsModel(req.body);
-            productToSave.save((error, response)=>{
+            productToSave.save((error, response) => {
                 if (error) {
                     return res.status(400).send({
                         ok: false,
@@ -36,9 +36,55 @@ async function addProduct(req, res) {
     } catch (error) {
         return res.status(500).send({
             ok: false,
-            error
+            error: 'Internal Server Error'
         });
     }
 }
 
-module.exports = { addProduct }
+async function updateProduct(req, res) {
+    try {
+        let productUpdated = await productsModel.findOneAndUpdate({ id: req.body.id }, req.body);
+        if (!productUpdated) {
+            return res.status(404).send({
+                ok: false,
+                response: 'Product not found'
+            })
+        } else {
+            return res.status(200).send({
+                ok: true,
+                productUpdated
+            });
+        }
+    } catch (error) {
+        return res.status(500).send({
+            ok: false,
+            error: 'Internal Server Error'
+        });
+    }
+}
+
+async function deleteProduct(req, res) {
+    try {
+        let productDeleted = await productsModel.findOneAndDelete({ id: req.body.id, pharmacy_id: req.body.pharmacy_id });
+        if (!productDeleted) {
+            return res.status(404).send({
+                ok: false,
+                response: 'Product not found'
+            })
+        } else {
+            return res.status(200).send({
+                ok: true,
+                productDeleted
+            });
+        }
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).send({
+            ok: false,
+            error: 'Internal Server Error'
+        });
+    }
+}
+
+module.exports = { addProduct, updateProduct, deleteProduct }
--- routes/products_routes.js
+++ routes/products_routes.js
@@ -4,5 +4,7 @@ const router = require('express').Router();
 const productController = require('../controllers/product')
 
 router.post('/add', productController.addProduct);
+router.put('/update', productController.updateProduct);
+router.delete('/delete', productController.deleteProduct);
 
 module.exports = router;
