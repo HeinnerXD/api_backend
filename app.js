06513d5647aed46a05825f76a347813e0567319b
Takeshi <heinner45@gmail.com>
2019-09-11 23:27:56 -0500

    headers

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 app.get('/', (req, res) => {
     return res.status(200).send({
         ok: true,
