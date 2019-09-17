e65697dceeb4e1d863115473bf64be1b33030861
Takeshi <heinner45@gmail.com>
2019-09-17 13:54:26 -0500

    cors policy

--- app.js
+++ app.js
@@ -6,19 +6,14 @@ const morgan = require('morgan');
 const products_routes = require('./routes/products_routes');
 const users_routes = require('./routes/user_routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+app.use(cors());
 
 app.get('/', (req, res) => {
     return res.status(200).send({
